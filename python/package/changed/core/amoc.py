0472b15ae96665198952420ff4f7b6421e601b83
Daniel Grose <31761607+grosed@users.noreply.github.com>
2023-05-29 14:48:51 +0100

    Update amoc.py

--- python/package/changed/core/amoc.py
+++ python/package/changed/core/amoc.py
@@ -3,5 +3,5 @@ from .add import add
 
 def amoc(f,R,beta) :
     a,b = min(R),max(R)
-    index,cost = split(f,R)
+    index,cost = split(f,f,R)
     return (index,cost + beta) if cost + beta < f(a,b) else (a,cost)
