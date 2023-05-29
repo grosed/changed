59f0a8fd8d8eb45fda3f48b252f16c8bacda1417
Daniel Grose <31761607+grosed@users.noreply.github.com>
2023-05-29 09:43:35 +0100

    Add files via upload

---
+++ python/package/changes/cost/normal/mean.py
@@ -0,0 +1,17 @@
+from itertools import accumulate
+
+class mean :
+    def __init__(self,X,minseglen=0) :
+        self.X = X
+        self.SX = [0] + list(accumulate(X))
+        self.SXX = [0] + list(accumulate([x*x for x in X]))
+        self.minseglen = minseglen
+    def __call__(self,a,b) :
+        if b - a + 1 < self.minseglen :
+            return inf
+        val = self.SX[b+1] - self.SX[a]
+        val *= val
+        val /= (b - a + 1)
+        val = -val
+        val += self.SXX[b+1] - self.SXX[a]
+        return val
