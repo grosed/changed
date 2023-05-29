d7e48fbe626a8ab334d94e63cf704973216ea616
Daniel Grose <31761607+grosed@users.noreply.github.com>
2023-05-29 15:06:51 +0100

    Delete amoc.py

--- python/package/changed/core/amoc.py
+++
@@ -1,7 +0,0 @@
-from .split import split
-from .add import add
-
-def amoc(f,g,R,beta) :
-    a,b = min(R),max(R)
-    index,cost = split(f,g,R)
-    return (index,cost + beta) if cost + beta < g(a,b) else (a,cost)
