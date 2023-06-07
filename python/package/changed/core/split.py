45bab6260ebcc9a4bc9534bdfe42a4bcc0021842
Daniel Grose <31761607+grosed@users.noreply.github.com>
2023-06-07 13:45:19 +0100

    Update split.py

--- python/package/changed/core/split.py
+++ python/package/changed/core/split.py
@@ -2,5 +2,5 @@ from .point_function import point_function
 
 def split(f,g,R) :
     a,d = min(R),max(R)
-    internal = [(c,f(a,b) + g(c,d)) for b,c in zip(R[:-1],R[1:])]
+    internal = [(b,f(a,b) + g(c,d)) for b,c in zip(R[:-1],R[1:])]
     return min([(a,g(a,d))] + internal + [(d,f(a,d))] ,key = lambda x : x[1])
