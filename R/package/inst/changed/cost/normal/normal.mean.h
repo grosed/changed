6cd8e730970022d5181cdb653cbb271543d4c3f5
Daniel Grose <31761607+grosed@users.noreply.github.com>
2023-06-14 10:12:01 +0100

    Add files via upload

--- R/package/inst/changed/cost/normal/normal.mean.h
+++ R/package/inst/changed/cost/normal/normal.mean.h
@@ -16,7 +16,7 @@ namespace changed
 	{
 	  sumstats<Ctype> S;  
 	  mean_template(const std::vector<Ctype>&);
-	  Ctype operator()(const Rtype&,const Rtype&);
+	  Ctype operator()(const Rtype&,const Rtype&) const;
 	};
       
       
@@ -24,7 +24,7 @@ namespace changed
 	mean_template<Rtype,Ctype>::mean_template(const std::vector<Ctype>& X) : S(X) {}
       
       template <typename Rtype,typename Ctype>
-	Ctype mean_template<Rtype,Ctype>::operator()(const Rtype& i,const Rtype& j)
+	Ctype mean_template<Rtype,Ctype>::operator()(const Rtype& i,const Rtype& j) const
 	{
 	  Ctype val = S.SX[j] - S.SX[i-1];
 	  val *= val;
