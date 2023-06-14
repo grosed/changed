c402943f2288d03debd476364ae8c87c41a1d676
Daniel Grose <31761607+grosed@users.noreply.github.com>
2023-06-14 13:13:26 +0100

    Add files via upload

--- R/package/inst/changed/cost/np/np.conditional.h
+++ R/package/inst/changed/cost/np/np.conditional.h
@@ -14,6 +14,7 @@ namespace changed
       template <typename Rtype,typename Ctype>
 	struct conditional_template
 	{
+	  std::vector<std::vector<Rtype> > S;
 	  conditional_template(const std::vector<Ctype>&, const Ctype&);
 	  Ctype operator()(const Rtype&,const Rtype&) const;
 	};
@@ -21,21 +22,43 @@ namespace changed
       template <typename Rtype,typename Ctype>
 	conditional_template<Rtype,Ctype>::conditional_template(const std::vector<Ctype>& X, const Ctype& k)
 	{
-	  // sot the data
+	  // sort the data
 	  auto n = X.size();
 	  auto XS = X;
 	  std::sort(std::begin(XS),std::end(XS));
-	  std::vector<int> I(k);
+	  std::vector<Rtype> I(k);
 	  auto c = -2.0*std::log(2*n-1);
 	  for(int i = 0; i < k; i++)
 	    {
-	      I[k] = (int)((n-1)*std::pow(1 + std::exp(c*(-1+2*(i+1)/k-1/k)),-1));
+	      I[k] = (Rtype)((n-1)*std::pow(1 + std::exp(c*(-1+2*(i+1)/k-1/k)),-1));
 	    }
+	  // calculate quantiles
 	  std::vector<Ctype> Q(k+2);
 	  auto infinity = std::numeric_limits<Ctype>::infinity();
 	  Q[0] = -infinity;
 	  Q[k+1] = infinity;
 	  std::transform(std::begin(I),std::end(I),std::begin(Q) + 1,[&XS](const auto& i){return XS[i];});
+	  S = std::vector<std::vector<Rtype> >(n+1);
+	  S[0] = std::vector<Rtype>(Q.size()-1,0);
+	  // indicate
+	  for(int i = 1; i <= n; i++)
+	    {
+	      S[i] = std::vector<Rtype>(Q.size()-1);
+	      std::transform(std::begin(Q) + 1,std::end(Q),std::begin(Q),std::begin(S[i]),
+			     [&X,&i](const auto& b,const auto& a)
+			     {
+			       return 1 ? a < X[i] && X[i] <= b : 0;
+			     }
+			     );
+	    }
+	  // accumalate 
+	  for(int i = 0; i < n; i++)
+	    {
+	      for(int j = 0; j < Q.size()-1; j++)
+		{
+		  S[i+1][j] += S[i][j]; 
+		}
+	    }
 	}
       
       
