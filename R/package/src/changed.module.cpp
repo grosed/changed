e1b9053b6d6ea2093b00a82ff624cf41958a28ad
Daniel Grose <31761607+grosed@users.noreply.github.com>
2023-06-14 10:10:55 +0100

    Add files via upload

--- R/package/src/changed.module.cpp
+++ R/package/src/changed.module.cpp
@@ -3,17 +3,30 @@
 #include <vector>
 
 #include "normal.mean.h"
+#include "np.conditional.h"
 #include "op.pruned.h"
 
 std::vector<int> cpt_normal_mean_impl(const std::vector<double>& X,const double& beta)
 {
-    return changed::methods::op_pruned<changed::cost::normal::mean>(X,beta);
+  changed::cost::normal::mean cf(X);
+  return changed::methods::op_pruned(cf,beta,X.size());
+}
+
+
+std::vector<int> cpt_np_conditional_impl(const std::vector<double>& X,
+					 const double& Q,
+					 const double& K,
+					 const double& beta)
+{
+  changed::cost::np::conditional cf(X,Q,K);
+  return changed::methods::op_pruned(cf,beta,X.size());
 }
 
 
 RCPP_MODULE(changed){
     using namespace Rcpp;
     function("cpt_normal_mean_impl",&cpt_normal_mean_impl,"");
+    function("cpt_np_conditional_impl",&cpt_np_conditional_impl,"");
 }
 
 
