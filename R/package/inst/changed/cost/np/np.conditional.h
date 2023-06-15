92d3d9360ec448ed411e42ccd33cf4208ef977bd
Daniel Grose <31761607+grosed@users.noreply.github.com>
2023-06-15 12:17:31 +0100

    Add files via upload

--- R/package/inst/changed/cost/np/np.conditional.h
+++ R/package/inst/changed/cost/np/np.conditional.h
@@ -19,6 +19,9 @@ namespace changed
 	  std::vector<std::vector<Rtype> > S;
 	  conditional_template(const std::vector<Ctype>&, const Ctype&);
 	  Ctype operator()(const Rtype&,const Rtype&) const;
+
+	  std::vector<Ctype> sumstats(const Rtype&,const Rtype&) const;
+	  
 	};
       
       template <typename Rtype,typename Ctype>
@@ -29,19 +32,23 @@ namespace changed
 	  auto XS = X;
 	  std::sort(std::begin(XS),std::end(XS));
 	  std::vector<Rtype> I(k);
-	  auto c = -2.0*std::log(2*n-1);
+	  auto c = std::log(2*n-1);
 	  for(int i = 0; i < k; i++)
 	    {
-	      I[i] = (Rtype)((n-1)*std::pow(1 + std::exp(c*(-1+2*(i+1)/k-1/k)),-1));
+	      auto yk = (-1+2*(i+1)/k-1/k);
+	      auto pk = std::pow(1 + std::exp(c*yk),-1);
+	      auto Ik = (Rtype)((n-1)*pk + 1);
+	      I[k-i-1] = Ik;
+	      // I[i] = (Rtype)((n-1)*std::pow(1 + std::exp(c*yk),-1)+1);
 	    }
 	  // calculate quantiles
 	  std::vector<Ctype> Q(k+2);
 	  auto infinity = std::numeric_limits<Ctype>::infinity();
 	  Q[0] = -infinity;
 	  Q[k+1] = infinity;
-	  std::transform(std::begin(I),std::end(I),std::begin(Q) + 1,[&XS](const auto& i){return XS[i];});
+	  std::transform(std::begin(I),std::end(I),std::begin(Q) + 1,[&XS](const auto& i){return XS[i-1];});
 	  S = std::vector<std::vector<Rtype> >(n+1);
-	  S[0] = std::vector<Rtype>(Q.size()-1,0);
+	  S[0] = std::vector<Rtype>(Q.size()-1,0);	  
 	  // indicate
 	  for(int i = 1; i <= n; i++)
 	    {
@@ -49,7 +56,7 @@ namespace changed
 	      std::transform(std::begin(Q) + 1,std::end(Q),std::begin(Q),std::begin(S[i]),
 			     [&X,&i](const auto& b,const auto& a)
 			     {
-			       return 1 ? a < X[i] && X[i] <= b : 0;
+			       return 1 ? a < X[i-1] && X[i-1] <= b : 0;
 			     }
 			     );
 	    }
@@ -68,9 +75,8 @@ namespace changed
 	Ctype conditional_template<Rtype,Ctype>::operator()(const Rtype& i,const Rtype& j) const
 	{
 	  
-	  Ctype t = (Ctype)(j + i - 1);
+	  Ctype t = (Ctype)(j - i + 1);
 	  std::vector<Ctype> M(S[0].size());
-	  //std::cout << i << " : " << j << " : ";
 	  std::transform(std::begin(S[j]),std::end(S[j]),std::begin(S[i-1]),std::begin(M),
 			 [&t](const auto& a, const auto& b)
 			 {
@@ -84,15 +90,49 @@ namespace changed
 			     {
 			       val = -m*std::log(m/t);
 			     }
+			   return val;
+			 }
+			 );
+	  auto val = std::accumulate(M.begin(),M.end(),0.0);
+	  return val;
+	}
+
+
+      template <typename Rtype,typename Ctype>
+	std::vector<Ctype> conditional_template<Rtype,Ctype>::sumstats(const Rtype& i,const Rtype& j) const
+	{
+	  Ctype t = (Ctype)(j - i + 1);
+	  std::vector<Ctype> M(S[0].size());
+	  //std::cout << i << " : " << j << " : ";
+	  std::transform(std::begin(S[j]),std::end(S[j]),std::begin(S[i-1]),std::begin(M),
+			 [&t](const auto& a, const auto& b)
+			 {
+			   auto m = a - b;
+			   double val;
+			   if(m == 0 || m == t)
+			     {
+			       val =  0.0;
+			     }
+			   else
+			     {
+			       // val = -m*std::log(m/t);
+			       val = m;
+			     }
 			   //		   std::cout << m << " -- " << t << " -- " << val << ",";
 			   return val;
 			 }
 			 );
 	  //std::cout << std::endl;
+	  return M;
+	  /*
 	  auto val = std::accumulate(M.begin(),M.end(),0.0);
 	  // std::cout << i << " : " << j << " : " << val << std::endl;
 	  return val;
+	  */
 	}
+
+
+      
       typedef conditional_template<int,double> conditional;
     } // namespace normal
   } // namespace cost
