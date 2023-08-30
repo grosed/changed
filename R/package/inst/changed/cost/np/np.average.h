b01ea344d0b264a0f439d0923575df04c271190b
Daniel Grose <31761607+grosed@users.noreply.github.com>
2023-08-30 08:54:02 +0100

    Update np.average.h

--- R/package/inst/changed/cost/np/np.average.h
+++ R/package/inst/changed/cost/np/np.average.h
@@ -54,6 +54,10 @@ namespace changed
       template <typename Rtype,typename Ctype>
 	Ctype average_template<Rtype,Ctype>::operator()(const Rtype& i,const Rtype& j) const
 	{	  
+	  if(i == j)
+	  {
+	     return 0.0;
+	  }
 	  Ctype t = (Ctype)(j - i + 1);
 	  std::vector<Ctype> M(S[0].size());
 	  std::transform(std::begin(S[j]),std::end(S[j]),std::begin(S[i-1]),std::begin(M),
